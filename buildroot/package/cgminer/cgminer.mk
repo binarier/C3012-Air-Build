7d0cfc970aff0b7c81fdc025166301b6c8379c7f
binarier <binarier@me.com>
2026-06-03 11:47:41 +0800

    更新cgminer脚本和代码位置

--- buildroot/package/cgminer/cgminer.mk
+++ buildroot/package/cgminer/cgminer.mk
@@ -5,14 +5,10 @@
 ################################################################################
 CGMINER_VERSION = $(call qstrip,$(BR2_PACKAGE_CGMINER_VERSION))
 CGMINER_SITE_METHOD = git
-CGMINER_SITE = git@git.highsharp.com:highsharp/cgminer.git
+CGMINER_SITE = https://github.com/binarier/cgminer.git
 CGMINER_DEPENDENCIES = host-pkgconf jansson libcurl
 CGMINER_AUTORECONF = YES
 CGMINER_CONF_OPTS = --enable-clover-rust --with-system-jansson 
-
-ifeq ($(BR2_PACKAGE_CGMINER_FPGA),y)
-CGMINER_CONF_OPTS += --enable-clover-fpga
-endif
 
 define BUILD_CGMINER_API
 	$(TARGET_CC) $(TARGET_CFLAGS) $(TARGET_LDFLAGS) -o $(@D)/cgminer-api \
@@ -25,6 +21,7 @@ endef
 
 CGMINER_POST_BUILD_HOOKS += BUILD_CGMINER_API
 #CGMINER_POST_EXTRACT_HOOKS += CGMINER_POST_EXTRACT
+CGMINER_CONF_ENV += CFLAGS="$(TARGET_CFLAGS) -fcommon"
 
 define INSTALL_CGMINER_API
 	$(INSTALL) -D -m 755 $(@D)/cgminer-api \
